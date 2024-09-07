4a078ab7beb52b798c0781bc714b64a5bd7b8118
Gabriel Melo <115416454+GabriielMelo@users.noreply.github.com>
2024-09-07 14:14:55 -0300

    feat: added initial header configs

---
+++ dalygames/src/components/header/index.tsx
@@ -0,0 +1,30 @@
+import logoImg from "@/public/logo.svg";
+import Image from "next/image";
+import Link from "next/link";
+import { LiaGamepadSolid } from "react-icons/lia";
+export default function Header() {
+  return (
+    <header className="w-full h-28 bg-slate-100 text-black px-2">
+      <div className="max-w-screen-lg-xl mx-auto flex justify-center items-center h-28">
+        <nav className="flex justify-center items-center gap-4 ">
+          <Link href={"/"}>
+            <Image
+              src={logoImg}
+              alt="Logo do site dalygames"
+              quality={100}
+              priority={true}
+              className="w-full"
+            ></Image>
+          </Link>
+          <Link href={"/"}>Games</Link>
+          <Link href={"/profile"}>Perfil</Link>
+        </nav>
+        <div className="hidden sm:flex">
+          <Link href={"/profile"}>
+            <LiaGamepadSolid size={34} color="#475569" />
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+}
